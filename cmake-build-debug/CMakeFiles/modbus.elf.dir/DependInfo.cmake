410dc1eb84bb5a7f61efabbc332b9244d310722a
MJC626 <88782127+MJC626@users.noreply.github.com>
2024-08-04 13:48:42 +0800

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/modbus.elf.dir/DependInfo.cmake
@@ -0,0 +1,72 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "ASM"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_ASM
+  "/Users/mjc626/STM32/modbus/Core/Startup/startup_stm32f411ceux.s" "/Users/mjc626/STM32/modbus/cmake-build-debug/CMakeFiles/modbus.elf.dir/Core/Startup/startup_stm32f411ceux.s.obj"
+  )
+set(CMAKE_ASM_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_ASM
+  "DEBUG"
+  "STM32F411xE"
+  "USE_HAL_DRIVER"
+  )
+
+# The include file search paths:
+set(CMAKE_ASM_TARGET_INCLUDE_PATH
+  "/Users/mjc626/STM32/modbus/Core/Inc"
+  "/Users/mjc626/STM32/modbus/Drivers/STM32F4xx_HAL_Driver/Inc"
+  "/Users/mjc626/STM32/modbus/Drivers/STM32F4xx_HAL_Driver/Inc/Legacy"
+  "/Users/mjc626/STM32/modbus/Drivers/CMSIS/Device/ST/STM32F4xx/Include"
+  "/Users/mjc626/STM32/modbus/Drivers/CMSIS/Include"
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/mjc626/STM32/modbus/Core/Src/adc.c" "CMakeFiles/modbus.elf.dir/Core/Src/adc.c.obj" "gcc" "CMakeFiles/modbus.elf.dir/Core/Src/adc.c.obj.d"
+  "/Users/mjc626/STM32/modbus/Core/Src/crc.c" "CMakeFiles/modbus.elf.dir/Core/Src/crc.c.obj" "gcc" "CMakeFiles/modbus.elf.dir/Core/Src/crc.c.obj.d"
+  "/Users/mjc626/STM32/modbus/Core/Src/dma.c" "CMakeFiles/modbus.elf.dir/Core/Src/dma.c.obj" "gcc" "CMakeFiles/modbus.elf.dir/Core/Src/dma.c.obj.d"
+  "/Users/mjc626/STM32/modbus/Core/Src/gpio.c" "CMakeFiles/modbus.elf.dir/Core/Src/gpio.c.obj" "gcc" "CMakeFiles/modbus.elf.dir/Core/Src/gpio.c.obj.d"
+  "/Users/mjc626/STM32/modbus/Core/Src/main.c" "CMakeFiles/modbus.elf.dir/Core/Src/main.c.obj" "gcc" "CMakeFiles/modbus.elf.dir/Core/Src/main.c.obj.d"
+  "/Users/mjc626/STM32/modbus/Core/Src/modbus.c" "CMakeFiles/modbus.elf.dir/Core/Src/modbus.c.obj" "gcc" "CMakeFiles/modbus.elf.dir/Core/Src/modbus.c.obj.d"
+  "/Users/mjc626/STM32/modbus/Core/Src/stm32f4xx_hal_msp.c" "CMakeFiles/modbus.elf.dir/Core/Src/stm32f4xx_hal_msp.c.obj" "gcc" "CMakeFiles/modbus.elf.dir/Core/Src/stm32f4xx_hal_msp.c.obj.d"
+  "/Users/mjc626/STM32/modbus/Core/Src/stm32f4xx_it.c" "CMakeFiles/modbus.elf.dir/Core/Src/stm32f4xx_it.c.obj" "gcc" "CMakeFiles/modbus.elf.dir/Core/Src/stm32f4xx_it.c.obj.d"
+  "/Users/mjc626/STM32/modbus/Core/Src/syscalls.c" "CMakeFiles/modbus.elf.dir/Core/Src/syscalls.c.obj" "gcc" "CMakeFiles/modbus.elf.dir/Core/Src/syscalls.c.obj.d"
+  "/Users/mjc626/STM32/modbus/Core/Src/sysmem.c" "CMakeFiles/modbus.elf.dir/Core/Src/sysmem.c.obj" "gcc" "CMakeFiles/modbus.elf.dir/Core/Src/sysmem.c.obj.d"
+  "/Users/mjc626/STM32/modbus/Core/Src/system_stm32f4xx.c" "CMakeFiles/modbus.elf.dir/Core/Src/system_stm32f4xx.c.obj" "gcc" "CMakeFiles/modbus.elf.dir/Core/Src/system_stm32f4xx.c.obj.d"
+  "/Users/mjc626/STM32/modbus/Core/Src/tim.c" "CMakeFiles/modbus.elf.dir/Core/Src/tim.c.obj" "gcc" "CMakeFiles/modbus.elf.dir/Core/Src/tim.c.obj.d"
+  "/Users/mjc626/STM32/modbus/Core/Src/usart.c" "CMakeFiles/modbus.elf.dir/Core/Src/usart.c.obj" "gcc" "CMakeFiles/modbus.elf.dir/Core/Src/usart.c.obj.d"
+  "/Users/mjc626/STM32/modbus/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal.c" "CMakeFiles/modbus.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal.c.obj" "gcc" "CMakeFiles/modbus.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal.c.obj.d"
+  "/Users/mjc626/STM32/modbus/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_adc.c" "CMakeFiles/modbus.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_adc.c.obj" "gcc" "CMakeFiles/modbus.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_adc.c.obj.d"
+  "/Users/mjc626/STM32/modbus/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_adc_ex.c" "CMakeFiles/modbus.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_adc_ex.c.obj" "gcc" "CMakeFiles/modbus.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_adc_ex.c.obj.d"
+  "/Users/mjc626/STM32/modbus/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_cortex.c" "CMakeFiles/modbus.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_cortex.c.obj" "gcc" "CMakeFiles/modbus.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_cortex.c.obj.d"
+  "/Users/mjc626/STM32/modbus/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma.c" "CMakeFiles/modbus.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma.c.obj" "gcc" "CMakeFiles/modbus.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma.c.obj.d"
+  "/Users/mjc626/STM32/modbus/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma_ex.c" "CMakeFiles/modbus.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma_ex.c.obj" "gcc" "CMakeFiles/modbus.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma_ex.c.obj.d"
+  "/Users/mjc626/STM32/modbus/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_exti.c" "CMakeFiles/modbus.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_exti.c.obj" "gcc" "CMakeFiles/modbus.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_exti.c.obj.d"
+  "/Users/mjc626/STM32/modbus/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash.c" "CMakeFiles/modbus.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash.c.obj" "gcc" "CMakeFiles/modbus.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash.c.obj.d"
+  "/Users/mjc626/STM32/modbus/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ex.c" "CMakeFiles/modbus.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ex.c.obj" "gcc" "CMakeFiles/modbus.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ex.c.obj.d"
+  "/Users/mjc626/STM32/modbus/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ramfunc.c" "CMakeFiles/modbus.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ramfunc.c.obj" "gcc" "CMakeFiles/modbus.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ramfunc.c.obj.d"
+  "/Users/mjc626/STM32/modbus/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_gpio.c" "CMakeFiles/modbus.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_gpio.c.obj" "gcc" "CMakeFiles/modbus.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_gpio.c.obj.d"
+  "/Users/mjc626/STM32/modbus/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr.c" "CMakeFiles/modbus.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr.c.obj" "gcc" "CMakeFiles/modbus.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr.c.obj.d"
+  "/Users/mjc626/STM32/modbus/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr_ex.c" "CMakeFiles/modbus.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr_ex.c.obj" "gcc" "CMakeFiles/modbus.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr_ex.c.obj.d"
+  "/Users/mjc626/STM32/modbus/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc.c" "CMakeFiles/modbus.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc.c.obj" "gcc" "CMakeFiles/modbus.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc.c.obj.d"
+  "/Users/mjc626/STM32/modbus/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc_ex.c" "CMakeFiles/modbus.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc_ex.c.obj" "gcc" "CMakeFiles/modbus.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc_ex.c.obj.d"
+  "/Users/mjc626/STM32/modbus/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim.c" "CMakeFiles/modbus.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim.c.obj" "gcc" "CMakeFiles/modbus.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim.c.obj.d"
+  "/Users/mjc626/STM32/modbus/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim_ex.c" "CMakeFiles/modbus.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim_ex.c.obj" "gcc" "CMakeFiles/modbus.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim_ex.c.obj.d"
+  "/Users/mjc626/STM32/modbus/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_uart.c" "CMakeFiles/modbus.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_uart.c.obj" "gcc" "CMakeFiles/modbus.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_uart.c.obj.d"
+  "/Users/mjc626/STM32/modbus/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_ll_adc.c" "CMakeFiles/modbus.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_ll_adc.c.obj" "gcc" "CMakeFiles/modbus.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_ll_adc.c.obj.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/modbus.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_ll_adc.c.obj.d
@@ -0,0 +1,2 @@
+CMakeFiles/modbus.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_ll_adc.c.obj: \
+ /Users/mjc626/STM32/modbus/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_ll_adc.c
